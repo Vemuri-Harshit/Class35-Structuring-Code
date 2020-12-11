ab672f5f276f55269d13d3aeac18735e71695e65
Vemuri-Harshit <73873977+Vemuri-Harshit@users.noreply.github.com>
2020-12-11 18:28:29 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,38 @@
+class Form{
+
+    constructor(){
+
+    }
+
+    display(){
+        var title = createElement('h2');
+        title.html("Multiplayer Game");
+        title.position(130,0);
+
+        var input = createInput("Name");
+        input.position(200,160);
+
+        var button = createButton('Play');
+        button.position(260,200);
+
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+
+            var name = input.value();
+            
+            playerCount+= 1;
+            player.update(name);
+            player.updateCount(playerCount);
+
+            var greeting = createElement('h3');
+            greeting.html("Hello " + name);
+            greeting.position(200,160);
+
+        })
+    }
+
+
+
+
+}
